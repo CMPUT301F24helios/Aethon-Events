697e2b4ad457ec6b1e4c28bcb4f768ee765f2bec
ak47me <azaveri@ualberta.ca>
2024-10-30 12:33:04 -0600

    merged malhar and abdulkadar branches

--- app/src/main/java/com/example/aethoneventsapp/MainActivity.java
+++ app/src/main/java/com/example/aethoneventsapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.aethoneventsapp;
 
+import androidx.annotation.Nullable;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -7,42 +8,74 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.example.aethoneventsapp.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
 public class MainActivity extends AppCompatActivity {
 
+    Button scan_btn;
+
+    TextView textView;
     private ActivityMainBinding binding;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-            switch (item.getItemId()) {
-                case R.id.Home:
-                    break;
-                case R.id.Profile:
-                    break;
-                case R.id.Events:
-                    break;
-                case R.id.Settings:
-                    break;
+        scan_btn = findViewById(R.id.scanner);
+        textView = findViewById(R.id.text);
 
+        scan_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                intentIntegrator.setOrientationLocked(false);
+                intentIntegrator.setPrompt("Scan a QR Code");
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                intentIntegrator.initiateScan();
             }
-            return true;
         });
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+//
+//            switch (item.getItemId()) {
+//                case R.id.Home:
+//                    break;
+//                case R.id.Profile:
+//                    break;
+//                case R.id.Events:
+//                    break;
+//                case R.id.Settings:
+//                    break;
+//
+//            }
+//            return true;
+//        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(intentResult != null){
+            String contents = intentResult.getContents();
+            if(contents != null){
+                textView.setText(intentResult.getContents());
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }

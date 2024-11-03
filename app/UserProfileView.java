3a2af5ae915c7546db8f0497ff3339b2f20b69aa
yashmouje <59363118+yashmouje@users.noreply.github.com>
2024-11-03 01:37:43 -0600

    UserProfile MVC Basic Structure

--- app/UserProfileView.java
+++ app/UserProfileView.java
@@ -1,3 +1,10 @@
 public class UserProfileView {
+    /**
+     * User Profile Page from deviceId
+     * @param deviceId
+     */
+    public void showUserProfile(String deviceId) {
+        // TO DO
+    }
 
 }

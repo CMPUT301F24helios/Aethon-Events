fd66fee2ab8e25e7a67b97fe220f784b032ac78a
yashmouje <59363118+yashmouje@users.noreply.github.com>
2024-11-06 22:38:15 -0700

    Creating Notification Service

---
+++ app/src/main/java/com/example/aethoneventsapp/AethonMessagingService.java
@@ -0,0 +1,9 @@
+package com.example.aethoneventsapp;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Aethon Events' Notification Service
+ */
+public class AethonMessagingService extends FirebaseMessagingService {
+}
